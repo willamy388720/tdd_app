569bc9774332098a62c6a54135b67c05417644dc
willamy388720 <willamygt@gmail.com>
2020-05-02 23:11:32 -0300

    Cadastro de cliente

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,6 +4,49 @@ class CustomersController < ApplicationController
   end
   
   def new
-    
+    @customer = Customer.new
   end
+
+  def edit
+  end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer.id), notice: 'Cliente atualizado com sucesso!'
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save
+      redirect_to customers_path, notice: 'Cliente cadastrado com sucesso!'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    if @customer.destroy
+      redirect_to customers_path, notice: 'Cliente excluído com sucesso!'
+    else
+      render :index
+    end
+  end
+
+private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:id, :name, :email, :smoker, :phone, :avatar)
+  end
+
 end
--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -19,4 +19,19 @@ feature "Customers", type: :feature do
     expect(page).to have_content('Novo Cliente')
   end
 
+  scenario 'Cadastra um cliente válido' do
+    visit(new_customer_path)
+    customer_name = Faker::Name.name
+
+    fill_in('customer_name', with: customer_name)
+    fill_in('customer_email', with: Faker::Internet.email)
+    fill_in('customer_phone', with: Faker::PhoneNumber.phone_number)
+    attach_file('customer_avatar', "#{Rails.root}/spec/fixtures/avatar.jpg")
+    choose(option: ['S', 'N'].sample)
+    click_on('Criar Cliente')
+
+    expect(page).to have_content('Cliente cadastrado com sucesso!')
+    expect(Customer.last.name).to eq(customer_name)
+  end
+
 end
